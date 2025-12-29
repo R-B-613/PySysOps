b59036f94ad917f6f6622636665beaf47d5aa2c6
sce <rb613m@gmail.com>
2025-12-29 10:44:00 +0200

    Added search module

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-# This is a sample Python script.
+import sys
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+if len(sys.argv) <2:
+    print("Usage: python3 main.py <filename>")
 
+if len(sys.argv) <3:
+    print("Usage: python3 main.py <filename> <number>")
+    sys.exit()
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+if len(sys.argv) <4:
+    print("Usage: python3 main.py <filename> <number> <number> <number>")
+    sys.exit()
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
